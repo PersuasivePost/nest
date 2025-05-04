d6287deeceb14572d2e19532d839b5f915d12a57
Ashvatth Joshi <codingash27@gmail.com>
2025-05-04 10:57:20 +0530

    auth logic added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Post, Req, Body, ParseIntPipe } from '@nestjs/common';
+//import { Request } from 'express';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
-export class AuthController{
-    constructor(private authService: AuthService) {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
 
-    @Post('signup')
-    signup(@Req() req: Request) {
-        console.log(req.body);
-        return this.authService.signup();
-    }
+  @Post('signup')
+  signup(
+    @Body('email') email: string,
+    @Body('password', ParseIntPipe) password: string,
+  ) {
+    console.log({
+      email,
+      typeOfEmail: typeof email,
+      password,
+      typeOfPassword: typeof password,
+    });
+    return this.authService.signup();
+  }
 
-    @Post('signin')
-    signin() {
-        return this.authService.signin();
-    }
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
 }
